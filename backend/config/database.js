5bb7248dc39bf0c2ce3c189c0040e222ab103d93
Collin Ehret <collin.ehret@gmail.com>
2024-10-09 14:21:49 -0700

    phase-0 save before CSRF Token access development

---
+++ backend/config/database.js
@@ -0,0 +1,30 @@
+// backend/config/database.js
+const config = require("./index");
+
+module.exports = {
+    development: {
+        storage: config.dbFile,
+        // username: "root",
+        // password: "cellphone",
+        // host: "127.0.0.1",
+        // port: 3306,
+        dialect: "sqlite",
+        seederStorage: "sequelize",
+        logQueryParameters: true,
+        typeValidation: true,
+    },
+    production: {
+        use_env_variable: "DATABASE_URL",
+        dialect: "postgres",
+        seederStorage: "sequelize",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+        define: {
+            schema: process.env.SCHEMA,
+        },
+    },
+};
